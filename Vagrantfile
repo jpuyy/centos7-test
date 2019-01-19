093f4952d52f00ac783c2492543a2f43e20dfd3c
jpuyy <jpuyy.com@gmail.com>
2019-01-19 21:59:56 +0800

    Forward some ports, install tools

--- Vagrantfile
+++ Vagrantfile
@@ -2,4 +2,6 @@ Vagrant.configure("2") do |config|
   config.vm.box = "centos/7"
   config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/id_rsa.pub"
   config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-yum install epel-release
+# using aliyun yum
+curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+# set hostname
+hostnamectl set-hostname centos7-work
+
+# epel
+yum install -y epel-release
+# basic tools
 yum install -y vim nc git expect traceroute oathtool
+# develop
+yum install -y mariadb npm
